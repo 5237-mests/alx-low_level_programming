29c94fe88597889f50cef4d806e548cf48205cca
5237-mests <99667573+5237-mests@users.noreply.github.com>
2022-03-11 17:29:58 +0300

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
 
+int main(void)
+{
+  char aA[52]="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  int i;
+  fir(i=0; i<52; i++)
+  {
+  putchar(aA[i]);
+  }
+  putchar('\n');
+  return(0);
+  
+}
