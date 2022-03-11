d4cd146eb3d7b129e017258fc4d24c8336524bf6
5237-mests <99667573+5237-mests@users.noreply.github.com>
2022-03-11 17:19:22 +0300

    Update 2-print.c

--- 0x01-variables_if_else_while/2-print.c
+++ 0x01-variables_if_else_while/2-print.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
 
+int main(void)
+{
+
+  char abc[26]="abcdefghijklmnopqrstuvwxyz";
+  int i; 
+  for(i=0; i<26; i++)
+  {
+    putchar(abc[i]);
+  }
+  putchar('\n');
+  return(0);
+  }
+}
