6571f7e7696936d75cbd71776db7a6b120a8f78f
5237-mests <99667573+5237-mests@users.noreply.github.com>
2022-03-11 18:53:53 +0300

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-/*
-*main function prints hexadecimal numbers in lowercase.
-*
-*return alwas give final of execution
-*/
-
+/**
+ * main - Prints numbers between 0 to 9 and letters between a to f.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int i;
