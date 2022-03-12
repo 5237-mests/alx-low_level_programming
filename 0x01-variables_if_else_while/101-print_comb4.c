a1f643dc34aee657abef8ac38d3ddf02921b7159
5237-mests <msfnmulgeta@gmail.com>
2022-03-12 12:37:49 +0300

    avd

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int i=48;
+  int j=48;
+  int x=48;
+  while(i<58)
+    {
+      j=48;
+      while(j<58)
+	{
+	  x=48;
+	  while(x<58)
+	    {
+	      if(i<j&&j<x)
+		{
+		  if(j<x)
+		    {
+		      putchar(i);
+		      putchar(j);
+		      putchar(x);
+
+		      if(x==57&&j==56)
+			{
+			  if(i==55)
+			    {
+			      break;
+			    }
+			}
+		      putchar(',');
+		      putchar(' ');
+		    }
+		}
+
+	      x++;
+	    }
+
+	  j++;
+	}
+	
+      i++;
+    }
+  putchar('\n');
+  return(0);
+}
---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int i=48;
+  int j=48;
+  int x=48;
+  int y=48;
+  int op1;
+  int op2;
+  while(i<58)
+    {
+      j=48;
+      while(j<58)
+	{
+	  x=48;
+	  while(x<58)
+	    {
+	      y=48;
+	      while(y<58)
+		{
+		  op1=i+j;
+		  op2=x+y;
+		  if(op1 < op2)
+		    {		      
+		      putchar(i);
+		      putchar(j);
+		      putchar(' ');		      
+		      putchar(x);
+		      putchar(y);
+
+		      if(y==57&&x==57&&j==56&&i==57)	    
+		       break;
+		       putchar(',');
+		       putchar(' ');
+		     
+		     }
+		  y++;
+		}
+	      x++;
+	    }
+	  j++;
+	}
+      i++;
+    }
+  putchar('\n');
+  return(0);
+}
