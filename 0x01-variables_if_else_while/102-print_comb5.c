f90be21b31b93ecfe673913da9c50352a85dd3f2
5237-mests <msfnmulgeta@gmail.com>
2022-03-13 16:25:55 +0300

    compiled

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -2,52 +2,49 @@
 
 /**
  * main - Prints numbers between 00 to 99.
- *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-  int i=48;
-  int j=48;
-  int x=48;
-  int y=48;
-  int op1;
-  int op2;
-  while(i<58)
-    {
-      j=48;
-      while(j<58)
-	{
-	  x=48;
-	  while(x<58)
-	    {
-	      y=48;
-	      while(y<58)
-		{
-		  op1=i+j;
-		  op2=x+y;
-		  if(op1 < op2)
-		    {		      
-		      putchar(i);
-		      putchar(j);
-		      putchar(' ');		      
-		      putchar(x);
-		      putchar(y);
-		      if(y==57&&x==57&&j==56&&i==57)
-		      {
-			      break;
-		      }
-		       putchar(',');
-		       putchar(' ');    
-		     }
-		  y++;
-		}
-	      x++;
-	    }
-	  j++;
-	}
-      i++;
-    }
-  putchar('\n');
-  return(0);
+int i = 48; int j = 48;
+int x = 48; int y = 48;
+int op1; int op2;
+
+while (i < 58)
+{
+j = 48;
+while (j < 58)
+{
+x = 48;
+
+while (x < 58)
+{
+y = 48;
+
+while (y < 58)
+{
+op1 = i + j;
+op2 = x + y;
+if (op1 < op2)
+{
+putchar(i);
+putchar(j);
+putchar(' ');
+putchar(x);
+putchar(y);
+if (y == 57 && x == 57 && j == 56 && i == 57)
+break;
+putchar(',');
+putchar(' ');
+}
+y++;
+}
+x++;
+}
+j++;
+}
+i++;
+}
+putchar('\n');
+return (0);
 }
