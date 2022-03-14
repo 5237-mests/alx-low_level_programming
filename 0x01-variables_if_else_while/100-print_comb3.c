bde8e772a04d2f80ef32fc69bd9d56c891d60d8e
5237-mests <msfnmulgeta@gmail.com>
2022-03-14 11:51:43 +0300

    edited

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,32 +1,34 @@
 #include <stdio.h>
+/**
+ *main - combinations of numbers
+ *
+ *Return: Always 0(success)
+ */
 
 int main(void)
 {
-  int i=48;
-  int j=48;
-  while(i<58)
-  {
-    int j=48;
-    while(j<58)
-    {
-      if(i!=j&&i<j)
-      {
-	putchar(i);
-	putchar(j);
-	if(j==57&&i==56)
-	{
-	  break;
-	}
-
-	putchar(',');
-	putchar(' ');
-      
-      }
-      j++;
-    }
-    i++;
-  }
-  putchar('\n');
-  return(0);
+int i = 48;
+int j = 48;
+while (i < 58)
+{
+int j = 48;
+while (j < 58)
+{
+if (i != j && i < j)
+{
+putchar(i);
+putchar(j);
+if (j == 57 && i == 56)
+{
+break;
 }
-  
+putchar(',');
+putchar(' ');
+}
+j++;
+}
+i++;
+}
+putchar('\n');
+return (0);
+}
