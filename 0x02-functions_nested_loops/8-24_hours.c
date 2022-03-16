77ad384fe17720e4eb2be28cb7948b880dcd38b3
5237-mests <99667573+5237-mests@users.noreply.github.com>
2022-03-16 16:54:40 +0300

    hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,1 +1,26 @@
+#include "main.h"
+/**
+ * jack_bauer - prints every minute of the day 00:00 to 23:59
+ * @void - no argument
+ */
+void jack_bauer(void)
+{
+	int i = 0, j;
+
+	while (i < 24)
+  {
+    j = 0;
+    while (j < 60)
+    {
+      _putchar(i / 10 + '0');
+      _putchar(i % 10 + '0');
+      _putchar(':');
+      _putchar(j / 10 + '0');
+      _putchar(j % 10 + '0');
+    j++;
+    }
+    _putchar('\n');
+    i++;
+  }
+}
 
