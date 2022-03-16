ef9891d1d4b1198cce49acfaf9b450ef07c758c8
5237-mests <99667573+5237-mests@users.noreply.github.com>
2022-03-16 16:40:54 +0300

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,1 +1,16 @@
+#include "main.h"
+/**
+ * print_last_digit - function that prints the last digit
+ * @n: takes ln an integer
+ * Return: returns output
+ */
+int print_last_digit(int n)
+{
+	int y= n % 10;
+
+	if (n < 0)
+		y = y * -1;
+	_putchar(ln + '0');
+	return (y);
+}
 
