e15e70a0788de46048586e2f1d5e016eaccf9a6a
5237-mests <99667573+5237-mests@users.noreply.github.com>
2022-03-16 16:20:55 +0300

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,1 +1,24 @@
+#include "holberton.h"
 
+/**
+ * print_alphabet_x10 - Prints the alphabet 10 times.
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0, j;
+  
+  while (i<10)
+  {
+    j = 97;
+    while (j < 123)
+    {
+      _putchar(j);
+      _putchar('\n');
+    j++;
+    }
+  _putchar('\n');
+  i++;
+  }
+}
