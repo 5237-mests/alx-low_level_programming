6d10d898ab0a4002bea6f4474052d04f587f32ac
5237-mests <99667573+5237-mests@users.noreply.github.com>
2022-03-18 16:11:54 +0300

    triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,1 +1,31 @@
+#include "main.h"
 
+/**
+ * print_triangle - prints a triangle.
+ * @size: numbers of lines.
+ * Return: no return.
+ */
+void print_triangle(int size)
+{
+  int i, j;
+  
+  i = size - 1;
+  
+  if (i = 0)
+  {
+    _putchar(35);
+  }
+  else if (i > 0)
+  {
+    for (j = 1; j < size; j++)
+    {
+      _putchar(' ');
+    }
+    _putchar(35);
+  }
+  else
+  {
+    _putchar('\n');
+  }
+	_putchar('\n');
+}
