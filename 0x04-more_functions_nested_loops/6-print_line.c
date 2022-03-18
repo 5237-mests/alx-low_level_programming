d00168cd0a05961ab65962c57c88f4a7b854cdd5
5237-mests <99667573+5237-mests@users.noreply.github.com>
2022-03-18 06:18:13 +0300

    print line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,16 +9,9 @@ void print_line(int n)
 {
 	int i;
 
-  if (n > 0)
-  {
-    for (i = 0; i < n; i++)
-    {
-      _putchar(95);
-      _putchar('\n');
-    }
-  }
-  else
-  {	
-    _putchar('\n');
-  }
+	for (i = 0; i < n; i++)
+	{
+		_putchar(95);
+	}
+	_putchar('\n');
 }
