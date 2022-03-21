4e295e050d6a05c387b27693d0b49a76c0c9cfa0
5237-mests <99667573+5237-mests@users.noreply.github.com>
2022-03-21 17:33:35 +0300

    reset to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,11 @@
 #include "main.h"
 
 /**
- *reset_to_98 - takes apointer to a parameter and update its value
- *@n:input integer
- *Return: no return
+ * reset_to_98 - takes a pointer to a parameter and updates its value.
+ * @n: input integer.
+ * Return: no return.
  */
-
 void reset_to_98(int *n)
 {
-*n = 98;
+	*n = 98;
 }
