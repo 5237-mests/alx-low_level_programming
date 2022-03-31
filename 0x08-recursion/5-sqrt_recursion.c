d53d0e46984bb1d5a555c4cc11cfba361230eec2
5237-mests <msfnmulgeta@gmail.com>
2022-03-31 15:08:15 +0000

    more recursion

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *-sqrt_recursion - gives natural square root
+ *@n: input num
+ *Return: square root of given num
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else
+		return ();
+}
