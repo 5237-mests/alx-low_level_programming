797d134067c81b3d97033a73a6064b3f2258fb5e
5237-mests <msfnmulgeta@gmail.com>
2022-04-11 14:25:44 +0000

    first dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - stores info about dog
+ *@name: dog's name
+ *@age: dog's age
+ *@owner: owner of dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
+
+
+
+
+
