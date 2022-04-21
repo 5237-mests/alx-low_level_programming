8d8571b8febee655f26a39a6fdeb19bc5671e0c5
5237-mests <99667573+5237-mests@users.noreply.github.com>
2022-04-22 00:04:11 +0300

    Update 1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,25 +1,19 @@
 #include "lists.h"
 /**
- * list_len - returns the number of elements in a list.
+ * list_len - returns then number of elements in a list.
  * @h: singly linked list.
  * Return: number of elements in the list.
  */
 
 size_t list_len(const list_t *h)
 {
-	int count = 0;
-  
-  if (h == NULL)
-    printf("empty");
-  list_t *ptr = NULL;
-  
-  ptr = h;
-  while (ptr != NULL)
-  {
-    count++;
-    ptr = ptr->next;    
-  }
-  printf("%d", count);
-	
-	return (count);
+	size_t nelem;
+
+	nelem = 0;
+	while (h != NULL)
+	{
+		h = h->next;
+		nelem++;
+	}
+	return (nelem);
 }
