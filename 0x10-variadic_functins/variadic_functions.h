1d24d95b4d95a2dc9932ce004135dc34d7c97b53
5237-mests <99667573+5237-mests@users.noreply.github.com>
2022-04-30 15:19:41 +0300

    Update variadic_functions.h

--- 0x10-variadic_functins/variadic_functions.h
+++ 0x10-variadic_functins/variadic_functions.h
@@ -1,15 +1,9 @@
-#ifndef VARIADIC_FUNCTIONS
-#define VARIADIC_FUNCTIONS
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
 
-#include <sdio.h>
-#include <stdarg.h>
-
-int _putchar(char);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
-void print_strings(const char *separator const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
-
 #endif
-
